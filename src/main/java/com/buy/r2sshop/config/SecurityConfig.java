316bc7fdf0bff12e3b61919fc587b237aca15cc9
hungtuan <hungtuan2072000@gmail.com>
2023-05-16 23:46:31 +0700

    test

--- src/main/java/com/buy/r2sshop/config/SecurityConfig.java
+++ src/main/java/com/buy/r2sshop/config/SecurityConfig.java
@@ -3,11 +3,11 @@ package com.buy.r2sshop.config;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class SecurityConfig {
-    public static String encode(String password) {
+    public static String encoder(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
-    public static boolean checkPW(String rawPassword, String encodedPassword) {
+    public static boolean check(String rawPassword, String encodedPassword) {
         return BCrypt.checkpw(rawPassword, encodedPassword);
     }
 }
