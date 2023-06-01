c2cdb58fcb4b3a6b177344892d2dcc5c347bf21c
hungtuan <hungtuan2072000@gmail.com>
2023-06-01 22:24:22 +0700

    add code

--- src/main/java/com/buy/r2sshop/config/SecurityConfig.java
+++ src/main/java/com/buy/r2sshop/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/v1/login").permitAll()
+                .requestMatchers("/v1/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
